15dab6054f791420f8b91cddb32eb65b394eebce
jtamu <gtamu.dip@gmail.com>
2025-11-01 19:06:14 +0900

    feat: recover from unhealthy

---
+++ src/chapter5/recover/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type startGoroutineFn func(
+	done <-chan interface{},
+	pulseInterval time.Duration,
+) (heartbeat <-chan interface{})
+
+func main() {
+	// or pattern from src/chapter4/goroutine_leak_fix_or_pattern.go
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+
+	newSteward := func(
+		timeout time.Duration,
+		startGoroutine startGoroutineFn,
+	) startGoroutineFn {
+		return func(
+			done <-chan interface{},
+			pulseInterval time.Duration,
+		) <-chan interface{} {
+			heartbeat := make(chan interface{})
+
+			go func() {
+				defer close(heartbeat)
+
+				var wardDone chan interface{}
+				var wardHeartbeat <-chan interface{}
+
+				startWard := func() {
+					wardDone = make(chan interface{})
+					wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
+				}
+				startWard()
+
+				pulse := time.Tick(pulseInterval)
+
+			monitorLoop:
+				for {
+					timeoutSignal := time.After(timeout)
+					for {
+						select {
+						case <-pulse:
+							select {
+							case heartbeat <- struct{}{}:
+							default:
+							}
+						case <-wardHeartbeat:
+							continue monitorLoop
+						case <-timeoutSignal:
+							log.Println("steward: ward unhealty; restarting")
+							close(wardDone)
+							startWard()
+							continue monitorLoop
+						case <-done:
+							return
+						}
+					}
+				}
+			}()
+			return heartbeat
+		}
+	}
+
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	dowork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
+		log.Println("ward: Hello, I'm irresponsible!")
+		go func() {
+			<-done
+			log.Println("ward: I am halting.")
+		}()
+		return nil
+	}
+
+	doworkWithSteward := newSteward(4*time.Second, dowork)
+
+	done := make(chan interface{})
+
+	time.AfterFunc(9*time.Second, func() {
+		log.Println("main: halting steward and ward.")
+		close(done)
+	})
+
+	for range doworkWithSteward(done, 4*time.Second) {
+	}
+	log.Println("Done.")
+}
